364c309a46db6393ff0db457c7c1e34bbbe913e2
marten <marten.tamm@gmail.com>
2019-05-12 19:24:25 +0300

    Minor fixes to the CurseFilter class

--- src/main/java/CurseFilter.java
+++ src/main/java/CurseFilter.java
@@ -12,10 +12,6 @@ import java.util.List;
 
 class CurseFilter {
 
-    List<String> cursesFromWeb;
-    List<String> cursesLocal;
-
-
     List<String> getCurseList() throws IOException {
         URL url = new URL("http://www.bannedwordlist.com/lists/swearWords.txt");
         InputStream inputStream = url.openStream();
@@ -32,7 +28,7 @@ class CurseFilter {
         return Files.readAllLines(Path.of("swearwords.txt"));
     }
 
-String replaceCurseWordsWithAsterisks(String message, List<String> cursesLocal, List<String> cursesFromWeb) throws IOException {
+    String replaceCurseWordsWithAsterisks(String message, List<String> cursesLocal, List<String> cursesFromWeb) {
         StringBuilder outgoingMessage = new StringBuilder();
         for (String string : message.split(" ")) {
             if (cursesLocal.contains(string.toLowerCase()) || cursesFromWeb.contains(string.toLowerCase())) {
@@ -59,14 +55,8 @@ String replaceCurseWordsWithAsterisks(String message, List<String> cursesLocal, 
         if (Files.notExists(Path.of("swearwords.txt")))
             return;
         List<String> curseList = Files.readAllLines(Path.of("swearwords.txt"));
-        try {
-            curseList.remove(word);
-            Files.write(Path.of("swearwords.txt"), curseList);
-            System.out.println("Curse word removed from blocked list!");
-        } catch (Exception e) {
-            System.out.println("No such word found!");
-
-        }
-
+        curseList.remove(word);
+        Files.write(Path.of("swearwords.txt"), curseList);
+        System.out.println("Curse word removed from blocked list!");
     }
 }
